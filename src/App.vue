f66affe804d48bb709daf5b8b8df97c4668d900f
Vahid Mohammadi <vahidm1467@gmail.com>
2022-06-28 05:59:47 +0430

    feat: implement the main structure

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,77 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="resume-wrapper">
+    <header>
+      <div class="avatar-wrapper">
+        <img
+          alt="Avatar"
+          class="avatar"
+          src="./assets/avatar.jpg"
+          width="125"
+          height="125"
+        />
+      </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div class="header-side">
+        <h1>Vahid Mohammadi</h1>
+        <div class="contacts">
+          <a href="https://www.linkedin.com/in/vahidmohammadi/">LinkedIn</a>
+          <a href="https://stackoverflow.com/users/1889607/vahid/"
+            >StackOverFlow</a
+          >
+          <a href="mailto:vahidm1467@gmail.com">vahidm1467@gmail.com</a>
+          <a href="tel:+989210329938">+989210329938</a>
+        </div>
+      </div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main class="container">
+      <div class="main-wrapper">
+        <article></article>
+        <aside></aside>
+      </div>
+    </main>
+  </div>
 </template>
 
 <style>
-@import './assets/base.css';
-
+@import "./assets/base.css";
+body {
+  background: #f9f9f9;
+}
 #app {
-  max-width: 1280px;
+  display: flex;
+  padding: 1rem;
+  background: #f9f9f9;
+}
+.resume-wrapper {background: #fff;
+  width: 1280px;
   margin: 0 auto;
   padding: 2rem;
-
-  font-weight: normal;
+  border-radius: 4px;
+  box-shadow: 0px 3.8px 5.3px rgba(0, 0, 0, 0.028),
+    0px 12.7px 17.9px rgba(0, 0, 0, 0.042), 0px 57px 80px rgba(0, 0, 0, 0.07);
+  padding: 1rem;
 }
 
 header {
-  line-height: 1.5;
+  display: flex;
 }
 
-.logo {
+.avatar-wrapper {
+}
+.avatar {
   display: block;
   margin: 0 auto 2rem;
 }
 
+.main-wrapper {
+  display: flex;
+}
 a,
 .green {
   text-decoration: none;
@@ -51,31 +86,10 @@ a,
 }
 
 @media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 }
 </style>
