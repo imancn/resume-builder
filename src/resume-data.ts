9ee8aa1a8efcfc065dcf7bd41e09255d14f4d02d
Iman Soltani <iman.soltani.j@gmail.com>
2025-05-09 14:29:56 +0330

    enhance bullets

--- src/resume-data.ts
+++ src/resume-data.ts
@@ -20,10 +20,10 @@ export const WORK_EXPERIENCES: WorkExperienceModel[] = [
         start: { month: '7', year: '2023' },
         desc: `
 <ul>
-<li>Architected and spearheaded a Spring Boot service to quantify performance of 6,000+ warehouse staff, infer bottlenecks, and dispatch alerts across ops channels.
-<li>Engineered a real-time tracker overseeing 340K+ daily product items awaiting shelf allocation.
-<li>Curated Metabase dashboards to illuminate flow states across 100+ fulfillment centers.
-<li>Eradicated +10 critical bugs root causes, slashing SQA tickets by 24%.
+<li>Led a team of 3 to architect and deliver a Spring Boot service ahead of deadline, quantifying performance for 6,000+ warehouse staff, identifying bottlenecks, and triggering alerts across operational channels.
+<li>Implemented a real-time tracker overseeing 340K+ daily product items awaiting shelf allocation.
+<li>Created Metabase dashboards to illuminate flow states across 100+ fulfillment centers.
+<li>Fixed +10 critical bugs root causes, caused SQA tickets reduces by 24%.
 </ul>`,
       },
     ],
@@ -41,14 +41,12 @@ export const WORK_EXPERIENCES: WorkExperienceModel[] = [
         // <p>After working at Tapsell for approximately 5 years, I leveraged my experience to establish and streamline processes, document tacit knowledge, develop developer handbooks, and implement a unified internal chat platform for the entire enterprise, spanning all products and teams across all Tapsell offices.</p>
         desc: `
 <ul>
-<li>Augmented 'Campaign Management' APIs and Data Stream pipelines for the Search Ads project (an OpenRTB DSP integration with CafeBazaar).
-<li>Forged the CafeBazaar Gateway to funnel event streams from 60M+ users into Kafka topics.
-<li>Orchestrated the RTB EventStash system to distill 700M+ daily events, synthesize signals, and archive analytics.
-<li>Devised and deployed an ETL pipeline that ingested raw advertising data, transformed it into structured analytics, and loaded it into a time-series database—enabling real-time performance tracking and trend analysis for 30K+ advertisers and publishers through interactive Kibana dashboards.
-<li>Automated daily financial margin reports across six ad network types through scheduled email delivery.
+<li>Developed the CafeBazaar Discovery Ad OpenRTB DSP integration services such as Gateway, EventStash to funnel event streams from 60M+ users into Kafka topics and to distill 700M+ daily events, synthesize signals, and archive analytics.
+<li>Devised and deployed an ETL pipeline that ingested advertising data, transformed it into structured analytics, and loaded it into a time-series database enabling tracking and trend analysis for 30K+ advertisers and publishers via interactive Kibana dashboards.
+<li>Automated daily financial margin reports across 6 Ad Network types through scheduled email delivery.
 <li>Chaired weekly sync-ups with the CafeBazaar engineering team to align cross-org tech initiatives.
+<li>Maitained 'Campaign Management' APIs and Data Stream pipelines for the CafeBazaar Search Ads project
 <li>Pioneered a browser extension surfacing live ad zone stats across 18K+ websites.
-<li>Remediated bugs, data anomalies, and operational issues across nine services.
 </ul>`,
       },
     ],
@@ -60,16 +58,14 @@ export const WORK_EXPERIENCES: WorkExperienceModel[] = [
     type: 'full-time',
     positions: [
       {
-        title: 'Junior Backend Developer',
+        title: 'Backend Developer',
         start: { month: '1', year: '2020' },
         end: { month: '4', year: '2021' },
         // <p>After working at Tapsell for approximately 5 years, I leveraged my experience to establish and streamline processes, document tacit knowledge, develop developer handbooks, and implement a unified internal chat platform for the entire enterprise, spanning all products and teams across all Tapsell offices.</p>
         desc: `
 <ul>
-<li>Published and maintained 40+ APIs in the 'User Service' to support user management across a distributed SOA ecosystem.
-<li>Optimized API performance, cutting response times by to 99%, and validated improvements with Apache JMeter load testing.
-<li>Authored comprehensive test suites, achieving 87% branch and 95% condition coverage pre-development.
-<li>Documented all APIs thoroughly, ensuring clarity and ease of integration for internal teams.
+<li>Published and maintained 40+ APIs in the 'User Service' to support user management in a distributed SOA ecosystem, with thorough documentation to ensure seamless internal integration.
+<li>Boosted API performance with up to 99% faster response times, validated via Apache JMeter load testing; developed comprehensive test suites achieving 87% branch and 95% condition coverage pre-development.
 </ul>`,
       },
     ],
@@ -84,7 +80,7 @@ export const WORK_EXPERIENCES: WorkExperienceModel[] = [
       {
         title: 'Co Founder',
         start: { month: '1', year: '2023' },
-        desc: `<p>Developing a platform name Lngujet to improving users language skills.</p>`,
+        desc: `<p>As Co-Founder of Langujet, I spearheaded the development of an AI-powered platform that democratizes access to high-quality language exam preparation. Recognizing the barriers posed by expensive and often subpar mock tests, we launched Langujet to offer free, expertly evaluated simulations for exams like IELTS, TOEFL, Duolingo, GRE, and PTE. Our platform combines advanced artificial intelligence with human expertise to provide accurate score estimations and personalized feedback, making language learning more accessible and effective for students worldwide.</p>`,
       },
     ],
   },
@@ -99,7 +95,7 @@ export const WORK_EXPERIENCES: WorkExperienceModel[] = [
         title: 'Software Engineer',
         start: { month: '6', year: '2019' },
         end: { month: '12', year: '2019' },
-        desc: `<ul><li>Developed a story based mini game.</li></ul>`,
+        desc: `<ul><li>As the solo developer and creator of Escaping Moon, I designed, built, and published a 2D arcade game using Unity and C#. The game challenges players to guide the moon through a chaotic asteroid field, combining fast-paced mechanics with minimalist space-themed visuals. Released in early 2020 on CafeBazaar, it has earned a 4.6-star rating from user reviews and over 100 downloads. I managed the full development cycle—from gameplay design and coding to graphic integration and publishing—gaining hands-on experience with Unity, object-oriented programming, Git, and mobile game deployment.</li></ul>`,
       },
     ],
   },
